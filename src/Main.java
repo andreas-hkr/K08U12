b9f07e4eebe869e3ef4b4c28727f370d6c8d774d
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-17 08:10:05 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] arg) {
+        // Beräkna sin(x) med hjälp av en Maclaurin-serie. Tricket är att inte beräkna
+        // varje term för sig utan att utgå från föregående (annars kommer det att ta lång tid).
+
+
+    }
+}
